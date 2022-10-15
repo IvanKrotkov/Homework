2d924ce780984154af3b8b217db642d3589e6570
Ivan <csgo.dr@mail.ru>
2022-10-15 17:27:37 +0700

    Выполнил задачу 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+int max = a;
+if(a>b)
+{
+    System.Console.WriteLine($"Максимальное число:{max}");
+}
+else 
+{
+    max = b;    
+    System.Console.WriteLine($"Максимальное число:{max}");
+}
