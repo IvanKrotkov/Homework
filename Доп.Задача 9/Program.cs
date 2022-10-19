55b138858a3898e5acd80131c5d9fe6a540c0b56
Ivan <csgo.dr@mail.ru>
2022-10-19 20:38:41 +0700

    Сделал доп задачу 9

---
+++ Доп.Задача 9/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает на вход число и 
+проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да
+*/
+string Diff(int num)
+{    
+    string i ="Да";
+    string m = "Нет";
+    if(num%7==0&&num%23==0)
+    {
+        return i;
+    }
+    else
+    {
+        return m;
+    }
+}
+System.Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(Diff(number));
