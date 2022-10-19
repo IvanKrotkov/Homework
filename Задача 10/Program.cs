afcd02c956873d7a8837dbc5a2e96ca3e7b36861
Ivan <csgo.dr@mail.ru>
2022-10-19 19:28:56 +0700

    Сделал Задачу 10

---
+++ Задача 10/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное
+число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int ThreeDigital(int number)
+{
+    if (number>100&&number<1000)
+    {
+        return ((number-number%10)%100)/10;
+    }
+    else
+    {
+        return 0;
+    }  
+}
+System.Console.WriteLine("Введите число");
+int InputNumber = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(ThreeDigital(InputNumber));
