cea64ebaf080d7ee8d382c6be6455869cc7f352d
Ivan <csgo.dr@mail.ru>
2022-10-19 20:02:45 +0700

    Сделал Задачу 13

---
+++ Задача 13/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+int ThirdDigital (int num)
+{
+     while (num>1000)
+    {
+      num = (num - num%10)/10;
+      
+    }
+    if (num<1000&&num>100)
+        {
+        return num%10;
+        }
+    
+    else 
+    {
+        System.Console.WriteLine("Третьей цифры нет");
+        return num;
+    }
+}
+
+System.Console.WriteLine("Введите число:");
+int InputNumber = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(ThirdDigital(InputNumber));
