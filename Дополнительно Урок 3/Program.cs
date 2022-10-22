f33b7b86944e247b77177113f171d14552545746
Ivan <csgo.dr@mail.ru>
2022-10-22 22:20:11 +0700

    не сделал доп задачу

---
+++ Дополнительно Урок 3/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая принимает на вход 
+число (N) и выдает таблицу простых чисел от 1 до N.
+11 -> 2, 3, 5 , 7, 11
+
+void SimpleNumber(int input)
+{
+ int count = 2;
+ while (count<input)
+ {
+    if (count%2==0 | count%3==0 | count%5==0 | count%7==0)
+    {
+        count++;
+    }
+    else 
+    {
+        System.Console.WriteLine(count);
+        count++;
+    }
+    count++;
+ }
+}
+
+System.Console.WriteLine("Введите число:");
+int inputNumber = int.Parse(Console.ReadLine()!);
+SimpleNumber(inputNumber);
+*/
