aa213ba3451bc384ee1b076c6a86e3811f896939
Ivan <csgo.dr@mail.ru>
2022-10-23 16:52:51 +0700

    сделал доп задачу

--- Дополнительно Урок 3/Program.cs
+++ Дополнительно Урок 3/Program.cs
@@ -1,26 +1,23 @@
 ﻿/* Напишите программу, которая принимает на вход 
 число (N) и выдает таблицу простых чисел от 1 до N.
-11 -> 2, 3, 5 , 7, 11
+11 -> 2, 3, 5 , 7, 11*/
 
 void SimpleNumber(int input)
 {
- int count = 2;
- while (count<input)
- {
-    if (count%2==0 | count%3==0 | count%5==0 | count%7==0)
+  for (int i = 2; i <= input; i++)
+  {
+    if (i%2!=0 && i%3!=0 && i%5!=0 && i%7!=0) 
     {
-        count++;
+        System.Console.WriteLine(i);
     }
-    else 
+    if(i==2||i==3||i==5||i==7)
     {
-        System.Console.WriteLine(count);
-        count++;
+        System.Console.WriteLine(i);
     }
-    count++;
- }
+  }
+ 
 }
 
 System.Console.WriteLine("Введите число:");
 int inputNumber = int.Parse(Console.ReadLine()!);
 SimpleNumber(inputNumber);
-*/
