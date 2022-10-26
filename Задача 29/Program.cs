e4f042217e8d1070a86144565ad554ca493bb548
Ivan <csgo.dr@mail.ru>
2022-10-26 16:45:24 +0700

    Исправил недочёты задачи 29

--- Задача 29/Program.cs
+++ Задача 29/Program.cs
@@ -5,8 +5,9 @@
 
 6, 1, 33 -> [6, 1, 33]*/
 
-void GetArray(int[] array, int lenght)
+void PrintArray(int[] array, int lenght)
 {
+    System.Console.Write("[");
     for (int j = 0; j < lenght; j++)
     {
         System.Console.Write(array[j]);
@@ -15,19 +16,22 @@ void GetArray(int[] array, int lenght)
             System.Console.Write(",");
         }
     }
+    System.Console.Write("]");
 }
 
-void Massive(int lenght)
+int[] CreateRandomArray(int lenght, int maxValue)
 {
     int[] array = new int[lenght];
     for (int i = 0; i < lenght; i++)
     {
-        array[i] = new Random().Next(0, 100);
+        array[i] = new Random().Next(0, maxValue);
     }
-    System.Console.Write("[");
-    GetArray(array, lenght);
-    System.Console.Write("]");
+    return array;
+  
 }
 System.Console.WriteLine("Введите длину массива:");
 int lenghtArray = int.Parse(Console.ReadLine()!);
-Massive(lenghtArray);
+System.Console.WriteLine("Введите число, которое будет границей диапазона на выбор случайного числа");
+int maxValue = int.Parse(Console.ReadLine()!);
+CreateRandomArray(lenghtArray,maxValue);
+PrintArray(CreateRandomArray(lenghtArray,maxValue), lenghtArray);
