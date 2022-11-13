64b98867d7c26b210ac5fa99b0485277dbeb8bc5
Ivan <csgo.dr@mail.ru>
2022-11-13 21:54:47 +0700

    Доп.Задача

---
+++ Доп.Задача Урок 5/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Дан массив чисел. Напишите функцию, которая ищет заданное число в 
+массиве и показывает нужный индекс в нем. При отсутствии числа возвращать -1.*/
+
+int NumberSearch(int[] arr, int searchNumber)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]==searchNumber)
+        {
+            System.Console.WriteLine("Индекс числа:"+i);
+            return i;
+        }
+        else if(i==arr.Length-1)
+        {
+            System.Console.WriteLine("Числа в массиве нет");
+        }
+    }
+    return -1;
+}
+
+int[] array = { 1, 14, 43 };
+NumberSearch(array, 43);
