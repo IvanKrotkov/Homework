95aeda953bd71788c87d5a32e877e18a75817ef5
Ivan <csgo.dr@mail.ru>
2022-11-13 20:35:37 +0700

    задача 34

---
+++ Двумерные массивы/Program.cs
@@ -0,0 +1,58 @@
+﻿/*string[,] table = new stringp[2,5];
+
+table[1,2] = "word";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        System.Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+*/
+
+
+
+/*void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+         for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        System.Console.Write($"{matrix[i, j]} ");
+    }
+    System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+
+FillArray(matrix);
+PrintArray(matrix);*/
+
+
+//f(1) =1 
+//f(2) =1 
+// f(n) = f(n-1)+f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    System.Console.WriteLine(Fibonacci(i));
+}
---
+++ Задача 34/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+int[] GetArray(int lengthArray)
+{
+    int[] array = new int[lengthArray];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+    return array;
+}
+
+void ParityEl(int[] array)
+{
+    int parity = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            parity++;
+        }
+    }
+    System.Console.WriteLine("Кол-во чётных чисел:" + parity);
+}
+
+ParityEl(GetArray(4));
