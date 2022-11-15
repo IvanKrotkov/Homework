57a9d1ca297f487b5a6fac0acf83b552c70f0896
Ivan <csgo.dr@mail.ru>
2022-11-15 13:18:51 +0700

    задача доп.Урок 6

---
+++ Доп. Задача Урок 6/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Дан массив. Найдите MEX массива. MEX (Minimum EXcluded) - минимальный отсутствующий элемент.*/
+
+int MEX(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        bool noFound = true;
+        for (int j = 0; j < array.Length; j++)
+        {
+            if (array[j] == i)
+            {
+                noFound = false;
+                break;
+            }
+        }
+        if (noFound)
+        {
+            return i;
+        }
+    }
+    return array.Length;
+}
+void PrintMex(int mex)
+{
+    System.Console.WriteLine(mex);
+}
+
+int[] array = { 0, 2, 3, 1, 4 };
+PrintMex(MEX(array));
+
